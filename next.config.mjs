132652d2ab3e4e993845bdfd5a0b20efad144e7c
Nandini Tandon <63950748+NandiniTandon21@users.noreply.github.com>
2024-09-02 00:03:00 +0530

    Add .gitignore file (#1)
    * Add .gitignore file
    
    * Added code files

---
+++ next.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  output: "standalone",
+};
+
+export default nextConfig;
---
+++ src/lib/nextauth.ts
@@ -0,0 +1,65 @@
+//Store configuration for auth
+
+import { DefaultSession, NextAuthOptions, getServerSession } from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import { prisma } from "./db";
+import GoogleProvider from "next-auth/providers/google";
+
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    user: {
+      id: string;
+    } & DefaultSession["user"];
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
+  }
+}
+export const authOptions: NextAuthOptions = {
+  session: {
+    strategy: "jwt",
+  },
+  //define callbacks - facilitate tokens and sessions
+  callbacks: {
+    jwt: async ({ token }) => {
+      const db_user = await prisma.user.findFirst({
+        where: {
+          email: token?.email,
+        },
+      });
+      if (db_user) {
+        token.id = db_user.id;
+      }
+      return token;
+    },
+    session: ({ session, token }) => {
+      if (token) {
+        session.user.id = token.id;
+        session.user.name = token.name;
+        session.user.email = token.email;
+        session.user.image = token.picture;
+      }
+      return session;
+    },
+  },
+
+  secret: process.env.NEXTAUTH_SECRET,
+
+  //interacting with users,using prisma- saving everything
+  //Need prisma adapter - install
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+};
+
+export const getAuthSession = () => {
+  return getServerSession(authOptions);
+};
